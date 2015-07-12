401cd00d61c3acafe94d4f8f74513ffc4a5b8729
humberts90 <humberts90@gmail.com>
2015-07-12 10:07:51 -0430

    Licencia GPL

--- Extensiones/Chrome/chrome/panel.js
+++ Extensiones/Chrome/chrome/panel.js
@@ -1,4 +1,12 @@
 /*
+    This Analizador-HTML5 consists of a tool for the analysis (lexical, syntactical and semantic) of HTML5 structures. The application was done purely in JavaScript, so it will not require an Internet connection to be used.
+
+    The project was presented as a thesis project for my university, so it only covers a portion of the W3C standard for HTML5 to date.
+
+    For the analysis, the W3C recommendation of October 28, 2014, was taken into account.
+
+    Copyright (C) 2015  Humberto Soler S.
+    
 panel.js is part of Analizador-HTML5.
 
     Analizador-HTML5 is free software: you can redistribute it and/or modify
