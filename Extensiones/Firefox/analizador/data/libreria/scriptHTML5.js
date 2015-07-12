feb3c3120c1eb75ac2cc4ea4f046f11ed8544bce
humberts90 <humberts90@gmail.com>
2015-07-12 09:48:09 -0430

    Licencia GPL

--- Extensiones/Firefox/analizador/data/libreria/scriptHTML5.js
+++ Extensiones/Firefox/analizador/data/libreria/scriptHTML5.js
@@ -1,4 +1,20 @@
+/*
+scriptHTML5.js is part of Analizador-HTML5.
 
+    Analizador-HTML5 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Analizador-HTML5 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Analizador-HTML.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
  'use stric';
  /**
   * Separates and returns the HTML tags present in the document. / Separa y retorna las etiquetas HTML presentes en el documento.
