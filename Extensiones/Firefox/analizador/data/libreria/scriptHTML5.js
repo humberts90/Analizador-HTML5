dcb0b28bdcf30e4daa94ceb3e00973a0ea2b707b
humberts90 <humberts90@gmail.com>
2015-07-20 22:54:43 -0430

    actualizando repo remoto

--- Extensiones/Firefox/analizador/data/libreria/scriptHTML5.js
+++ Extensiones/Firefox/analizador/data/libreria/scriptHTML5.js
@@ -142,7 +142,7 @@ function analizarHTML (parser, html) {
    		var saltos = '';
    		if(numR.length > 1){
    			
-   			for(var i =0; i<numR.length-1;i++){
+   			for(var i =0; i<numR.length;i++){
    				saltos += "\n";
    			}
    			
@@ -156,7 +156,7 @@ function analizarHTML (parser, html) {
    }); // convierte el codigo de una misma etiqueta, en 1 sola completa con saltos de linea la diferencia
    
    var lineasFuente = fuente.match(
-		new RegExp("(\n*[^\n]*)", "gmi")
+		new RegExp("(\n*[^\n^\r]*)", "gmi")
 		); //each line becomes a position in the array / convierte cada linea en una posicion en el vector
  
 	resultAnalisis = traducirLinea(lineasFuente,htmlOriginal,htmlOriginal2,objAST.getAdvertencia); // parse output translates to the real numbers of the document / se traduce la salida del parse a la numeración real del documento
